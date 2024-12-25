6135a2cc83b212afb4100265d1bffdc6cccdff62
himanshumainwal <mainwal.himanshu1994@gmail.com>
2024-12-26 00:24:40 +0530

    modified toggle burger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,14 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const [mobileMenuOpen, setmMobileMenuOpen] = useState(false)
+
+  const closeMenu = () => {
+    setmMobileMenuOpen(false);
+  };
+
+
   return (
-    <header className='sticky opacity-75 top-0 z-50 w-full bg-gradient-to-r from-[#b91c1c] to-[#7f1d1d] hover:from-[#7f1d1d] hover:to-[#b91c1c] px-4 lg:px-0 text-white'>
+    <header className='fixed opacity-75 top-0 z-50 w-full bg-gradient-to-r from-[#b91c1c] to-[#7f1d1d] hover:from-[#7f1d1d] hover:to-[#b91c1c] px-4 lg:px-0 text-white'>
       <div className=' max-w-7xl mx-auto flex h-14 items-center'>
         <div className='md:mr-4 flex justify-between w-full'>
           <Link to="/" className='mr-6 flex items-center space-x-2'>
@@ -69,10 +75,10 @@ const Navbar = () => {
       {mobileMenuOpen && (
         <div className='md:hidden'>
           <div className='space-y-1 bg-gradient-to-r from-[#b91c1c] to-[#7f1d1d] px-2 pb-3 pt-2 h-screen absolute right-0 w-2/3 rounded-bl-2xl'>
-            <Link to="/" className='block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-50 hover:text-red-700'>Home</Link>
-            <Link to="/about" className='block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-50 hover:text-red-700'>About</Link>
-            <Link to="/projects" className='block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-50 hover:text-red-700'>Project</Link>
-            <Link to="/contact" className='block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-50 hover:text-red-700'>Contact</Link>
+            <Link to="/" onClick={closeMenu} className='block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-50 hover:text-red-700'>Home</Link>
+            <Link to="/about" onClick={closeMenu} className='block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-50 hover:text-red-700'>About</Link>
+            <Link to="/projects" onClick={closeMenu} className='block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-50 hover:text-red-700'>Project</Link>
+            <Link to="/contact" onClick={closeMenu} className='block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-50 hover:text-red-700'>Contact</Link>
           </div>
         </div>
       )}
