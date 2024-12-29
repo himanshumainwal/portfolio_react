7d23b059941e81998830f144e39252ff43a6b931
himanshumainwal <mainwal.himanshu1994@gmail.com>
2024-12-29 16:26:25 +0530

    add more pojects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,25 +5,29 @@ import firebase_auth_demo from '../assets/project_Images/firebase_auth_demo.png'
 import netflix_Clone from '../assets/project_Images/netflix_Clone.png'
 import password_Generator from '../assets/project_Images/password_Generator.png'
 import pdf_Browse_App from '../assets/project_Images/pdf_Browse_App.png'
+import Calculator from '../assets/project_Images/Calculator.png'
+import TicTacToe from '../assets/project_Images/TicTacToe.png'
+import movieSearch from '../assets/project_Images/movieSearch.png'
 import Cards from './Cards'
 
 
 const Projects = () => {
 
   const projectJson = [
-    {
-      title: 'Password Generator',
-      desc: 'A responsive React app for generating secure, customizable passwords with options for length, numbers, and special characters.',
-      image: password_Generator,
-      live: "https://curious-chimera-d723ca.netlify.app/",
-      github: "https://github.com/himanshumainwal/password_Generator"
-    },
+
     {
       title: 'Netflix Clone',
       desc: `A responsive React app mimicking Netflix's UI, featuring navigation, a homepage, and styled sections for movies and shows.`,
       image: netflix_Clone,
       live: "https://taupe-semifreddo-b78566.netlify.app/",
       github: "https://github.com/himanshumainwal/netflix_Clone"
+    },
+    {
+      title: 'TicTacToe Game',
+      desc: 'TicTacToe is an interactive two-player game to align three symbols in a row, column, or diagonal on a 3x3 grid.',
+      image: TicTacToe,
+      live: "https://unrivaled-nougat-0ad8d8.netlify.app/",
+      github: "https://github.com/himanshumainwal/TicTacToe"
     },
     {
       title: 'Currency Converter',
@@ -40,11 +44,11 @@ const Projects = () => {
       github: "https://github.com/himanshumainwal/pdf-browse-app"
     },
     {
-      title: 'Sundown copy',
-      desc: 'A visually captivating replica of the Sundown website, showcasing responsive design and attention to detail.',
-      image: sundown,
-      live: "https://neon-entremet-65b1cf.netlify.app/",
-      github: "https://github.com/himanshumainwal/SunDown"
+      title: 'Calculator',
+      desc: 'A simple and intuitive calculator app designed to perform basic arithmetic operations effortlessly.',
+      image: Calculator,
+      live: "https://zingy-jalebi-148e04.netlify.app/",
+      github: "https://github.com/himanshumainwal/Calculator"
     },
     {
       title: 'firebase_auth_demo',
@@ -52,6 +56,27 @@ const Projects = () => {
       image: firebase_auth_demo,
       live: "https://charming-clafoutis-85488f.netlify.app/",
       github: "https://github.com/himanshumainwal/firebase-auth-demo"
+    },
+    {
+      title: 'Movie Search',
+      desc: `Movie Search is a web app to explore and find movie details effortlessly by searching titles.`,
+      image: movieSearch,
+      live: "https://scintillating-seahorse-b5c604.netlify.app/",
+      github: "https://github.com/himanshumainwal/movieSearch"
+    },
+    {
+      title: 'Sundown copy',
+      desc: 'A visually captivating replica of the Sundown website, showcasing responsive design and attention to detail.',
+      image: sundown,
+      live: "https://neon-entremet-65b1cf.netlify.app/",
+      github: "https://github.com/himanshumainwal/SunDown"
+    },
+    {
+      title: 'Password Generator',
+      desc: 'A responsive React app for generating secure, customizable passwords with options for length, numbers, and special characters.',
+      image: password_Generator,
+      live: "https://curious-chimera-d723ca.netlify.app/",
+      github: "https://github.com/himanshumainwal/password_Generator"
     },
 
   ];
